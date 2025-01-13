db350d186cece3977878fc82c4a4647164d8ff1e
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-01-12 23:48:18 -0600

    cars page

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,3 +7,24 @@ export type Brand = {
     height: number;
   };
 };
+
+export type Model = {
+  id: number;
+  name: string;
+  brand: Brand;
+};
+
+export type Image = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+export type Car = {
+  id: number;
+  model: Model;
+  images: Image[];
+  description: string;
+  year: string;
+  is_available: boolean;
+};
