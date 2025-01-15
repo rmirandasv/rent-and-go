bba19345d42ea0df8a9555c6b30c53ebb0ae67e8
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-01-14 20:37:50 -0600

    update styles

--- src/app/brands/page.tsx
+++ src/app/brands/page.tsx
@@ -5,7 +5,7 @@ import BrandCard from "@/components/brand-card/brand-card";
 export default async function BrandsPage() {
   const brands = await getBrands();
   return (
-    <div className="container py-16">
+    <div className="px-4 md:px-8 lg:px-10 py-16">
       <h1 className="text-4xl font-bold mb-8 text-center">Our Brands</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {brands.data.map((brand: Brand) => (
--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -5,7 +5,7 @@ import CarCard from "@/components/car-card/car-card";
 export default async function CarsPage() {
   const apiCars = await getCars();
   return (
-    <div className="container py-16">
+    <div className="px-4 md:px-8 lg:px-10 py-16">
       <h1 className="text-4xl font-bold mb-8 text-center">Our Cars</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {apiCars.data.map((car: Car) => (
