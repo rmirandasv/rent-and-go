574f0e274b22641f89b1054bc7ec092df70f0cd8
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-01-16 15:19:02 -0600

    remove next-auth to implement manual cookie base session sync with backend

--- src/components/header/user-menu.tsx
+++ src/components/header/user-menu.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-import { User } from "@/types";
 import { UserIcon } from "lucide-react";
-import { signOut } from "next-auth/react";
-import { useEffect, useState } from "react";
 
 export default function UserMenu() {
-  const [user, setUser] = useState<User | null>(null);
-
-    useEffect(() => {
-    async function fetchSession () {
-      const response = await fetch("/api/auth/session");
-      const data = await response.json();
-      if (data?.user) {
-        setUser(data.user);
-      }
-    }
-    fetchSession();
-  }, []);
-
-  if (!user) return null;
-
   return (
     <div className="py-2 px-4 flex items-center space-x-2">
       <UserIcon size={24} />
-      <span className="text-sm font-semibold">{user.name}</span>
-      <button className="text-sm text-blue-600" onClick={() => signOut()}>Sign out</button>
+      <span className="text-sm font-semibold">Ronald Miranda</span>
     </div>
   );
 }
--- src/components/rent-car-link/rent-car-link.tsx
+++ src/components/rent-car-link/rent-car-link.tsx
@@ -1,25 +1,11 @@
 "use client";
 import { Car } from "@/types";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function RentCarLink({ car }: { car: Car }) {
-  const [href, setHref] = useState<string>(`/cars/${car.documentId}/rent`);
-  useEffect(() => {
-    async function fetchSession () {
-      const response = await fetch("/api/auth/session");
-      const data = await response.json();
-      if (data?.user) {
-        setHref(`/cars/${car.documentId}/rent`);
-      } else {
-        setHref(`/api/auth/signin?callbackUrl=/cars/${car.documentId}/rent`);
-      }
-    }
-    fetchSession();
-  }, [car.documentId]);
   return (
     <Link
-      href={href}
+      href={`/cars/${car.documentId}/rent`}
       className="block bg-green-600 text-white p-4 text-center rounded-lg text-lg uppercase font-bold"
     >
       Rent this car
