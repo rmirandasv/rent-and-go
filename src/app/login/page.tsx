7d1b7119bc39dc55905b5169eb713dbb4202a9d7
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-01-16 21:15:57 -0600

    login with google page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,26 @@
+import logo from "@/app/logo.webp";
+import Image from "next/image";
+import Link from "next/link";
+import { STRAPI_URL } from "@/config/strapi";
+import google from "./google.svg";
+
 export default async function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+      <Link
+        href={`${STRAPI_URL}/api/connect/google`}
+        className="mt-8 p-4 bg-white border border-gray-300 text-center rounded-lg text-lg uppercase font-bold flex items-center space-x-4 shadow hover:shadow-lg"
+      >
+        <Image src={google} alt="Google" width={24} height={24} />
+        <span>Login with Google</span>
+      </Link>
     </div>
   );
 }
